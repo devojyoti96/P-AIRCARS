22b1a56b23f9175691edabd0e846e672f93f0de8
devojyoti96 <devojyoti96@gmail.com>
2021-04-27 00:14:41 +0530

    Debugged

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+import os
+
+os.system('cp -r scripts/run_intensity_selfcal.py scripts/run_intensity_selfcal')
+os.system('cp -r scripts/run_bandpass_selfcal.py scripts/run_bandpass_selfcal')
+os.system('cp -r scripts/run_pol_selfcal.py scripts/run_pol_selfcal')
 
 setup(
     name='paircars',
@@ -7,10 +12,12 @@ setup(
     author='Devojyoti Kansabanik',
     author_email='Andrew.Williams@curtin.edu.au',
     description='PAIRCARS',
-    scripts=['scripts/run_intensity_selfcal.py','scripts/run_bandpass_selfcal.py','scripts/run_pol_selfcal.py'],
+    scripts=['scripts/run_intensity_selfcal','scripts/run_bandpass_selfcal','scripts/run_pol_selfcal'],
     install_requires=["numpy", "astropy", "skyfield", "matplotlib", "scipy>=0.15.1", "h5py"],
     extras_require={'skymap':["ephem", "Pillow"]}   # Needed only to generate sky maps in mwa_pb/skymap.py
 )
+
+os.system('rm -rf scripts/run_intensity_selfcal scripts/run_bandpass_selfcal scripts/run_pol_selfcal')
 
 setup(
     name='paircars_casatasks',
@@ -27,7 +34,7 @@ setup(
     name='CALIBRATE',
     version='1.1.0',
     packages=['CALIBRATE'],
-    package_data={'CALIBRATE':['calibrate_tools/*.fits']},
+    package_data={'CALIBRATE':['calibrate_tools/*']},
     author='Devojyoti Kansabanik',
     author_email='Andrew.Williams@curtin.edu.au',
     description='PAIRCARS',
