5accfe2f8ebd9ae5cf72df4e53a091434fa6f9d1
devojyoti96 <devojyoti96@gmail.com>
2021-11-09 01:12:01 +0530

    modified

--- docs/conf.py
+++ docs/conf.py
@@ -52,7 +52,7 @@ htmp_theme_options={'display_version': True}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 
 
