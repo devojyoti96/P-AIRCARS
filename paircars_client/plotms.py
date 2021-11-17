a5f1925412feaaf511f28a8d97f7ab7d79040ab0
devojyoti96 <devojyoti96@gmail.com>
2021-11-17 18:14:36 +0530

    P-AIRCARS remote access client

--- paircars_client/plotms.py
+++ paircars_client/plotms.py
@@ -1,13 +1,21 @@
-import os,glob,subprocess
+import os,glob,subprocess,numpy as np
+from casatools import msmetadata
 
-
-def plotms(msname='',xaxis='TIME',yaxis='amp',xdatacolumn='DATA',ydatacolumn='DATA',iteration='',ants='',baseline='',spw='',chan='',field=0,scan=1,corr=''):
+def plotms(msname='',savedir='',plotflag=False,savelog=True,xaxis='TIME',yaxis='amp',xdatacolumn='DATA',ydatacolumn='DATA',\
+			iteration='',ants='',baseline='',spw='',chan='',field=0,scan=1,corr=''):
 	'''
 	Function to MS data using shadems
+	NOte : error checks for wrong input parameters has not been implemented, so please give correct inputs.
 	Parameters
 	----------
 	msname : str
 		Measurement set name
+	savedir : str
+		Directory to save final png files (default : current directory)
+	plotflag : bool
+		Plot flagged data or not (default : False)
+	savelog : bool
+		Save log file or not (default : False)
 	xaxis : str
 		X axis (TIME, CHAN, FREQ, CORR, U, V, W, UV, 'amp', 'phase', 'real', 'imag')
 	yaxis : str
@@ -30,15 +38,35 @@ def plotms(msname='',xaxis='TIME',yaxis='amp',xdatacolumn='DATA',ydatacolumn='DA
 		Scan number
 	corr : str
 		Correction or Stokes, 'XX,XY,YX,YY,RR,RL,LR,LL,I,Q,U,V'	
+	Returns
+	-------
+	list
+		Output image name list
+	int
+		Success message (0 or 1)
 	'''	
-	shadems_args=['shadems','--xaxis='+xdatacolumn+':'+xaxis,'--yaxis='+ydatacolumn+':'+yaxis]
-	if iteration!='':
-		shadems_args.append('--iter-'+iteration)
+	cwd=os.getcwd()
+	if msname=='':
+		print ('No measurement set is given.\n')
+		return [],1
+	else:
+		os.chdir(msname) 
+	if xaxis=='amp' or xaxis=='phase' or  xaxis=='real' or xaxis=='imag':
+		shadems_args=['shadems','--xaxis='+xdatacolumn+':'+xaxis]
+	else:
+		shadems_args=['shadems','--xaxis='+xaxis]
+	if yaxis=='amp' or yaxis=='phase' or  yaxis=='real' or yaxis=='imag':
+		shadems_args.append('--yaxis='+ydatacolumn+':'+yaxis)
+	else:
+		shadems_args.append('--yaxis='+yaxis)
+	if iteration!='' and iteration!=None:
+		if iteration!='None':
+			shadems_args.append('--iter-'+iteration)
 	if ants!='':
 		shadems_args.append('--ant-num='+ants)
 	if baseline!='':
 		shadems_args.append('--baseline='+baseline)
-	if spw='':
+	if spw!='':
 		shadems_args.append('--spw='+spw)
 	if chan!='':
 		shadems_args.append('--chan='+chan)
@@ -46,9 +74,76 @@ def plotms(msname='',xaxis='TIME',yaxis='amp',xdatacolumn='DATA',ydatacolumn='DA
 		shadems_args.append('--field='+str(field))
 	if scan!=1:
 		shadems_args.append('--scan='+str(scan))
-	
-
-
+	if corr!='':
+		shadems_args.append('--corr='+corr)
+	md=msmetadata()
+	md.open(msname)
+	nrows=int(md.nrows())
+	nchan=md.nchan(0)
+	chunk_size=int(nrows/3)
+	shadems_args.append('-j0')
+	shadems_args.append('--cmap=kb')
+	shadems_args.append('--no-lim-save')
+	shadems_args.append('-z'+str(chunk_size))
+	if xaxis=='CHAN' or xaxis=='FREQ' or yaxis=='CHAN' or yaxis=='FREQ':
+		if nchan<=1 and xaxis=='CHAN':
+			shadems_args.append('--xmin=-50')
+			shadems_args.append('--xmax=50')
+		elif nchan<=1 and xaxis=='CHAN':
+			shadems_args.append('--ymin=-50')
+			shadems_args.append('--ymax=50')
+		elif nchan<=1 and xaxis=='FREQ':
+			freq=md.chanfreqs(0)[0]
+			freqres=md.chanres(0)[0]
+			shadems_args.append('--xmin='+str(freq-5*freqres))
+			shadems_args.append('--xmax='+str(freq+5*freqres))
+		elif nchan<=1 and yaxis=='FREQ':
+			freq=md.chanfreqs(0)[0]
+			freqres=md.chanres(0)[0]
+			shadems_args.append('--ymin='+str(freq-5*freqres))
+			shadems_args.append('--ymax='+str(freq+5*freqres))
+		if xaxis=='CHAN' and nchan>1:
+			shadems_args.append('--colour-by=CHAN')
+			shadems_args.append('--cnum='+str(nchan))
+		elif xaxis=='FREQ' and nchan>1:
+			minfreq=np.min(md.chanfreqs(0))
+			maxfreq=np.max(md.chanfreqs(0))
+			freqres=md.chanres(0)[0]
+			shadems_args.append('--xmin='+str(minfreq-5*freqres))
+			shadems_args.append('--xmax='+str(maxfreq+5*freqres))
+	md.close()
+	if savedir!='':
+		if os.path.isdir(savedir)==False:
+			try:
+				os.makedirs(savedir)
+				savedir=savedir
+			except:
+				savedir=cwd
+				pass
+		else:
+			savedir=savedir
+	else:
+		savedir=cwd
+	shadems_args.append('--dir='+savedir)
+	if plotflag==True:
+		shadems_args.append('--noflags')
+	shadems_args.append('--fontsize=30')
+	shadems_args.append(msname)
+	shadems_cmd=' '.join(shadems_args)
+	if os.path.exists('log-shadems.txt'):
+		os.system('rm -rf log-shadems.txt')
+	a=os.system(shadems_cmd)
+	if a!=0:
+		return [],1
+	outputname=[]
+	with open('log-shadems.txt','r') as fil:
+		for line in fil:
+			if 'wrote ' in line:
+				outputname.append(line.split('wrote ')[-1].split('\n')[0])
+	if savelog==False:
+		os.system('rm -rf '+savedir+'/log-shadems.txt '+savedir+'/casa*log')
+	os.chdir(cwd)
+	return outputname,0
 
 
 
--- paircars_client/run_carta.py
+++ paircars_client/run_carta.py
@@ -1,9 +1,14 @@
 import os,glob,jprq,time,numpy as np
+import paircars_client
 
-screen_path='/usr/bin'
+datadir=os.path.abspath(os.path.dirname(paircars_client.__file__))
+try:
+	screen_path=str(np.load(datadir+'/screen_path.npy',allow_pickle=True))
+except:
+	screen_path='/usr/bin'
 os.path.join(screen_path)
-
 def get_carta_url(basedir=''):
+	os.environ['APPIMAGE_EXTRACT_AND_RUN']='1'
 	if basedir!='':
 		if basedir[-1]=='/':
 			basedir=basedir[:-1]
@@ -13,13 +18,16 @@ def get_carta_url(basedir=''):
 				for line in fil1:
 					if 'CARTA is accessible at ' in line:
 						carta_url=line.split('CARTA is accessible at ')[-1].split('\n')[0]
-			carta_port=carta_url.split('/?')[0].split(':')[-1]
-			os.system('fuser -k -n tcp '+str(carta_port))
-			del carta_port
+			try:
+				carta_port=carta_url.split('/?')[0].split(':')[-1]
+				os.system('fuser -k -n tcp '+str(carta_port))
+				del carta_port
+			except:
+				pass
 			os.system('rm -rf '+basedir+'/cartalog')
 		if os.path.exists(basedir+'/cartaerror'):
 			os.system('rm -rf '+basedir+'/cartaerror')
-		os.system('nohup ./CARTA-v2.0-redhat.AppImage --no_browser --top_level_folder '+basedir+' '+basedir+' > '+basedir+'/cartalog 2> '+basedir+'/cartaerror < /dev/null &')
+		os.system('nohup '+datadir+'/CARTA.AppImage --no_browser --top_level_folder '+basedir+' '+basedir+' > '+basedir+'/cartalog 2> '+basedir+'/cartaerror < /dev/null &')
 		time.sleep(5)
 		with open(basedir+'/cartalog','r') as fil1:
 			for line in fil1:
@@ -27,15 +35,18 @@ def get_carta_url(basedir=''):
 					carta_url=line.split('CARTA is accessible at ')[-1].split('\n')[0]
 		carta_port=carta_url.split('/?')[0].split(':')[-1]
 		carta_token='/?'+carta_url.split('/?')[-1]
-		if os.path.exists('tcp.output'):
-			os.system('rm -rf tcp.output')
+		if os.path.exists(basedir+'/tcp.output'):
+			os.system('rm -rf '+basedir+'/tcp.output')
+		cwd=os.getcwd()
+		os.chdir(basedir)
 		screen_cmd='jprq tcp '+carta_port
 		screen_name=os.path.basename(basedir)+'_carta_screen'
 		os.system('screen -S '+screen_name+' -X quit')
 		os.system('screen -mdS '+screen_name)
 		os.system('screen -S '+screen_name+' -X stuff "'+screen_cmd+'\n"')
+		os.chdir(cwd)
 		time.sleep(5)
-		with open('tcp.output','r') as fil2:
+		with open(basedir+'/tcp.output','r') as fil2:
 			for line in fil2:
 				if 'tcp.jprq.io' in line:
 					carta_public_url=line.split(', ')[-1].split('\n')[0]+carta_token
