9530c00cb4af8df1f56ceecac8959dba75a0e366
devojyoti <devojyoti96@gmail.com>
2026-02-21 21:31:35 +0100

    remove warnings

--- paircars/pipeline/__init__.py
+++ paircars/pipeline/__init__.py
@@ -1,63 +1,18 @@
-# ---------------------------------------------------------
-# Global warning & logging policy for PairCARS
-# ---------------------------------------------------------
-
-import warnings
-import logging
 import os
 
-# -----------------------------
-# Suppress Prefect / pydantic noise
-# -----------------------------
-warnings.filterwarnings(
-    "ignore",
-    message=".*pyproject_toml_table_header.*",
-)
+os.environ["PYTHONWARNINGS"] = "ignore"
+import logging
 
-warnings.filterwarnings(
-    "ignore",
-    message=".*toml_file.*",
-)
-
-# -----------------------------
-# Suppress tzlocal deprecated timezone warning
-# -----------------------------
-warnings.filterwarnings(
-    "ignore",
-    message=".*timezone is deprecated.*",
-)
-
-# -----------------------------
-# Optional: suppress generic UserWarnings from libraries
-# (comment out if you want stricter behavior)
-# -----------------------------
-warnings.filterwarnings("ignore", category=UserWarning)
-
-# ---------------------------------------------------------
-# Reduce noisy third-party logging
-# ---------------------------------------------------------
 logging.getLogger("distributed").setLevel(logging.ERROR)
-logging.getLogger("distributed.scheduler").setLevel(logging.ERROR)
-logging.getLogger("distributed.worker").setLevel(logging.ERROR)
 logging.getLogger("tornado.application").setLevel(logging.CRITICAL)
 logging.getLogger("tzlocal").setLevel(logging.ERROR)
-logging.getLogger("prefect").setLevel(logging.ERROR)
-
-# ---------------------------------------------------------
-# CASA log cleanup
-# ---------------------------------------------------------
 from casatasks import casalog
 
 try:
     logfile = casalog.logfile()
-    if os.path.exists(logfile):
-        os.remove(logfile)
-except Exception:
+    os.remove(logfile)
+except BaseException:
     pass
-
-# ---------------------------------------------------------
-# Astropy IERS configuration (offline safe)
-# ---------------------------------------------------------
 from astropy.utils import iers
 
 iers.conf.auto_download = False
