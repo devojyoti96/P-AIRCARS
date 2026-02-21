e96698dece68a1de79eb0e285e50cebd86e65384
devojyoti <devojyoti96@gmail.com>
2026-02-21 22:11:38 +0100

    remove warnings

--- paircars/clusterutils/slurm_cluster.py
+++ paircars/clusterutils/slurm_cluster.py
@@ -343,7 +343,7 @@ def submit_slurm_master_flow(args, jobid):
             for script_arg in script_args:
                 f.write(f"{script_arg}\n")
         print(f"Batch script: {script_path} is ready for submission.")
-        subprocess.run(["sbatch", script_path])
+        result = subprocess.run(["sbatch", script_path])
         exit_code = result.returncode
         return 0 if exit_code == 0 else 1
     except Exception as e:
