4f347e7f392d8fd7f97f6c5026f5d8ed2cf97d43
devojyoti <devojyoti96@gmail.com>
2026-02-21 21:46:08 +0100

    remove warnings

--- paircars/pipeline/run_paircars.py
+++ paircars/pipeline/run_paircars.py
@@ -1,4 +1,9 @@
 import os
+os.environ["PYTHONWARNINGS"] = "ignore"
+import logging
+logging.getLogger("distributed").setLevel(logging.ERROR)
+logging.getLogger("tornado.application").setLevel(logging.CRITICAL)
+logging.getLogger("tzlocal").setLevel(logging.ERROR)
 import sys
 import traceback
 import argparse
