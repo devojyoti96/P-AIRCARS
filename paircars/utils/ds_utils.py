85c1a5d4d320a8fdf2de5c8733f149cc6374fb66
devojyoti <devojyoti96@gmail.com>
2026-02-28 12:23:57 +0100

    Fixing selfcal issues related to flagging

--- paircars/utils/ds_utils.py
+++ paircars/utils/ds_utils.py
@@ -648,7 +648,7 @@ def calc_dynamic_spectrum(msname, metafits, outdir, n_threads=-1):
         S_sun[:, t] = s_sun_interpolated
 
     T_sun[np.isnan(T_sun)]=0
-    flags = np.where(np.isnan(T_sun)==True)
+    flags = np.where(T_sun <= 0) 
 
     save_file = f"freq_{mid_freq}MHz_time_{t_string}"
     np.save(
