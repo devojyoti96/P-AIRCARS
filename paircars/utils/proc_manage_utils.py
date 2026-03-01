984b139d226a76ac9722fad9bead7c984654a441
devojyoti <devojyoti96@gmail.com>
2026-03-02 00:27:07 +0100

    Modifying notification system to notify failed and successful ms

--- paircars/utils/proc_manage_utils.py
+++ paircars/utils/proc_manage_utils.py
@@ -442,7 +442,7 @@ def submit_local_master_flow(args, jobid):
         with open(script_path, "w") as f:
             for script_arg in script_args:
                 f.write(f"{script_arg}\n")
-        with open(log_file, "w") as log:
+        with open(logfile, "a", buffering=1) as log:
             process = subprocess.Popen(
                 ["bash", script_path],
                 stdout=subprocess.PIPE,
@@ -454,6 +454,7 @@ def submit_local_master_flow(args, jobid):
             for line in process.stdout:
                 sys.stdout.write(line)   # show in terminal
                 log.write(line)          # write to file
+                log.flush()
 
             process.stdout.close()
             exit_code = process.wait()
