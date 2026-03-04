4a4343457ea9b296d173562c909ac5adace20fb7
devojyoti <devojyoti96@gmail.com>
2026-03-04 18:42:13 +0100

    Bandpass merging test

--- paircars/utils/calibration.py
+++ paircars/utils/calibration.py
@@ -99,6 +99,9 @@ def merge_caltables(caltables, merged_caltable, append=False, keepcopy=False):
                 tb.open(caltable)
                 tb.copyrows(merged_caltable)
                 tb.close()
+                tb.open(f"{caltable}/SPECTRAL_WINDOW")
+                tb.copyrows(f"{merged_caltable}/SPECTRAL_WINDOW")
+                tb.close()
                 if not keepcopy:
                     os.system("rm -rf " + caltable)
     return merged_caltable
