67a8cbf550cf655b1f9b74f8bce523bd9de33fd8
devojyoti <devojyoti96@gmail.com>
2026-03-10 11:23:09 +0100

    Fixing overlay with cacheing sunpy maps

--- paircars/pipeline/make_mwa_overlay.py
+++ paircars/pipeline/make_mwa_overlay.py
@@ -179,6 +179,7 @@ def main(
                     euv_fits,
                     workdir,
                     plot_file_prefix=os.path.basename(img).replace(".fits", ""),
+                    verbose=True,
                 )
             )
 
