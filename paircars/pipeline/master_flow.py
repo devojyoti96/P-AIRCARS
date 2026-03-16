5e4b6d0f071c4c59452e9207f925ad232d1373ad
devojyoti <devojyoti96@gmail.com>
2026-03-16 23:26:24 +0100

    Selfcal flagging towards improving

--- paircars/pipeline/master_flow.py
+++ paircars/pipeline/master_flow.py
@@ -2956,7 +2956,7 @@ def master_control(
                 prefix=prefix,
                 force_split=True,
                 only_disk=True,
-                time_window=min(time_window, time_interval),
+                time_window=min(1.0, time_interval),
                 time_interval=time_interval,
                 quack_timestamps=quack_timestamps,
                 jobid=jobid,
